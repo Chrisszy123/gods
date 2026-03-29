2da35e3f4fc0193dc65b6142008c935edb750bad
chrisszy123 <bassygoodluck@gmail.com>
2026-03-29 02:42:22 +0100

    feat: add MetaPixel component for Facebook tracking and integrate it into the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
 import localFont from "next/font/local";
+import MetaPixel from "@/components/MetaPixel";
 
 
 // === BRAND FONTS ===
@@ -37,6 +37,7 @@ export default function RootLayout({
       <body
         className={`${cogsBolts.variable} ${nexa.variable} antialiased`}
       >
+        <MetaPixel />
         {children}
       </body>
     </html>
